2aae3a420e6b5dfb52e4019692547bc3b1028c9b
chohyeryung <s2019w38@e-mirim.hs.kr>
2021-01-11 22:38:20 +0900

    실습 1_1

--- ex1/includes_e.js
+++ ex1/includes_e.js
@@ -1,9 +1,10 @@
 function print(){
     let list=document.querySelectorAll("li");
-    let listArray=Array.from(list);
+    let listArray=Array.from(list);//li노드로 구성된 배열
     let eArray=listArray.filter(function(v){
-        return v.innerText.includes("e");
+      return v.innerText.includes("e");
     });
     console.log(eArray.length);
-}
-print();
+  }
+  
+  print();
